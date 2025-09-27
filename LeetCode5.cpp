1b78b5f365ba96f905fa2dbf949292f52193f2da
Syedahsan-2000 <107259633+Syedahsan-2000@users.noreply.github.com>
2025-09-26 22:48:22 -0500

    Floyd finding Cycle Linked List

---
+++ LeetCode5.cpp
@@ -0,0 +1,31 @@
+// detecting a Cycle using Floyd's cycle algorithm
+
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+        if (head == nullptr || head->next == nullptr) {
+            return nullptr;
+        }
+
+        ListNode *slow = head;
+        ListNode *fast = head;
+
+       
+        while (fast != nullptr && fast->next != nullptr) {
+            slow = slow->next;
+            fast = fast->next->next;
+
+            if (slow == fast) {  
+                
+                slow = head;
+                while (slow != fast) {
+                    slow = slow->next;
+                    fast = fast->next;
+                }
+                return slow;
+            }
+        }
+
+        return nullptr;
+    }
+};
